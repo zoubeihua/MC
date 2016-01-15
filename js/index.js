5e3ac7db23d90df96f7f7a5fdaca43f20434f83c
邹倍华 <zoubeihua@shjxgame.com>
2016-01-15 16:15:23 +0800

    修改了主页js相关的跳转页面！

---
+++ js/index.js
@@ -0,0 +1,32 @@
+window.onload = function(){
+	      		//获得slider插件对象
+	var slider = mui("#slider");
+	slider.slider({
+	  interval:2000//自动轮播周期，若为0则不自动播放，默认为0；
+	});
+	
+	mui('#offCanvasSideScroll').scroll();
+	mui('#offCanvasContentScroll').scroll();
+	
+	document.getElementById("binding").addEventListener('tap',function(){
+		mui.openWindow({
+	    url:"/ttqq/mobile/userbinding.html",
+	   	show:{
+	   		autoShow:true,
+	   		aniShow:"slide-in-right",
+	   		duration:300,
+	   	}
+	});	
+	});
+	
+	document.getElementById("taskindex").addEventListener('tap',function(){
+		mui.openWindow({
+	    url:"/ttqq/mobile/taskindex.html",
+	   	show:{
+	   		autoShow:true,
+	   		aniShow:"slide-in-right",
+	   		duration:300,
+	   	}
+	});	
+	});
+}
